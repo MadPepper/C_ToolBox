0b6ee363186fa839e1d245bef17beed702aff30d
MadPepperJP <hyper.pepper+github@github.com>
2018-01-12 10:29:53 +0900

    Add File io tools.

---
+++ src/FileTools.h
@@ -0,0 +1,43 @@
+/**
+ * FileTools.h
+ * @brief	ファイル入出力とかディレクトリ操作とか
+ * @note
+ */
+
+#ifndef FILETOOLS_H_
+#define FILETOOLS_H_
+/*------------------------------------------------*
+ * Includes
+ *------------------------------------------------*/
+#include <stdio.h>
+#include "TypeDef.h"
+
+/*------------------------------------------------*
+ * Defines/Macros
+ *------------------------------------------------*/
+#define D_FT_MAX_ENT_NUM		(1024)				/// フォルダ内の最大エントリ数（これ以上は扱わない）
+#define D_FT_MAX_FILE_PATH_LEN	(512+FILENAME_MAX)	/// ファイルパスの最大長（これ以上は扱わない）
+#define D_FT_DELIMITER			('\\')				/// Windowsのパスのデリミタ
+
+/*------------------------------------------------*
+ * Type definitions
+ *------------------------------------------------*/
+/**
+ * @brief	ディレクトリ内容を格納するための構造体
+ * @note
+ */
+typedef struct{
+	ULONG ulCount;													/// エントリ数
+	CHAR aascFileStr[D_FT_MAX_ENT_NUM][FILENAME_MAX];				/// ディレクトリ名orファイル名
+	CHAR aascFilePathStr[D_FT_MAX_ENT_NUM][D_FT_MAX_FILE_PATH_LEN];	/// パス
+}T_FT_FILES_ARRAY;	///
+
+/*------------------------------------------------*
+ * Function prototype
+ *------------------------------------------------*/
+LLONG FT_LoadFile(CHAR *pcFilePath, CHAR **pcLoadBuffer);
+LONG FT_CompareFile(CHAR *pcFilePath1, CHAR *pcFilePath2);
+LONG FT_GetDirectoryPath(CHAR *pcInputPath, CHAR *pcOutputPath);
+LONG FT_GetEntriesInDirectory(CHAR *pcDirectoryPath, T_FT_FILES_ARRAY *ptEntryArray);
+
+#endif /* FILETOOLS_H_ */
---
+++ src/TypeDef.h
@@ -0,0 +1,54 @@
+/*
+ * TypeDef.h
+ * @brief	Œ^‚ÌÄéŒ¾
+ * @note	CO_TARGET_OS_TYPE‚ğ•ÏX‚·‚é‚±‚Æ‚ÅA‘ÎÛOSã‚ÌŒ^‚ÆŒİŠ·«‚Ì‚ ‚éŒ`‚ÅéŒ¾‚·‚é
+ */
+
+#ifndef TYPEDEF_H_
+#define TYPEDEF_H_
+/*------------------------------------------------*
+ * Includes
+ *------------------------------------------------*/
+
+/*------------------------------------------------*
+ * Defines/Macros
+ *------------------------------------------------*/
+#define D_OS_TYPE_32BIT			(0)
+#define D_OS_TYPE_64BIT			(1)
+
+#define TRUE					(1)
+#define FALSE					(0)
+
+#define CO_TARGET_OS_TYPE		(D_OS_TYPE_32BIT)
+
+/*------------------------------------------------*
+ * Type definitions
+ *------------------------------------------------*/
+typedef char					CHAR;		// 8bit signed integer
+typedef unsigned char			BYTE;		// 8bit unsigned integer
+typedef char					INT8;		// 8bit signed integer
+typedef unsigned char			UINT8;		// 8bit unsigned integer
+typedef signed short			SHORT;		// 16bit signed integer
+typedef unsigned short			USHORT;		// 16bit unsigned integer
+typedef signed short			INT16;		// 16bit signed integer
+typedef unsigned short			UINT16;		// 16bit unsigned integer
+#if (CO_TARGET_OS_TYPE == D_OS_TYPE_32BIT)
+typedef signed int				LONG;		// 32bit signed integer
+typedef unsigned int			ULONG;		// 32bit unsigned integer
+#else
+typedef signed long				LONG;		// 64bit signed integer
+typedef unsigned long			ULONG;		// 64bit unsigned integer
+#endif
+typedef signed int				INT32;		// 32bit sigend integer
+typedef unsigned int			UINT32;		// 32bit unsigned integer
+typedef signed int				BOOL;		// 32bit signed integer
+typedef float					FLOAT;		// 32bit floating point
+typedef signed long long		LLONG;		// 64bit signed integer
+typedef unsigned long long		ULLONG;		// 64bit unsigned integer
+typedef double					DOUBLE;		// 64bit floating point
+
+/*------------------------------------------------*
+ * Function prototype
+ *------------------------------------------------*/
+
+#endif /* TYPEDEF_H_ */
